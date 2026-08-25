da19cdd146cb7d43b997f04ad552e06c882d12c9
n2hn8stwtn-sys <n2hn8stwtn@privaterelay.appleid.com>
2026-08-25 21:32:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("myButton");
+
+button.addEventListener("click", function() {
+    alert("Ты нажала на кнопку! 💚");
+});
+
+const themeButton = document.getElementById("themeButton")
+
+themeButton.addEventListener("click", function() {
+    document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+        themeButton.textContent = "☀️ Светлая тема";
+    } else {
+        themeButton.textContent = "🌙 Тёмная тема";
+    }
+});
+
+const menuButton = document.getElementById("menuButton"); 
+const nav = document.querySelector("header nav"); 
+
+menuButton.addEventListener("click", function() { 
+    nav.classList.toggle("active"); 
+});
